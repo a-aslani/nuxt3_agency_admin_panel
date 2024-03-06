c0cceafaca63c3a8dbd3f4c9921cea5548d2aaac
Alireza Aslani <c.computerdev@yahoo.com>
2024-03-06 10:26:01 +0330

    fix: fixed navigation drawer item click issue

--- components/AppNavigationDrawer.vue
+++ components/AppNavigationDrawer.vue
@@ -13,7 +13,7 @@
     </v-list>
     <v-divider></v-divider>
     <v-list density="compact" nav>
-      <v-list-item to="/" prepend-icon="mdi-view-dashboard" title="Dashboard" value="dashboard"></v-list-item>
+      <v-list-item to="/" prepend-icon="mdi-view-dashboard" title="Dashboard"></v-list-item>
 
       <v-list-group prepend-icon="mdi-account-multiple">
         <template v-slot:activator="{props}">
@@ -31,17 +31,17 @@
       <v-list-group prepend-icon="mdi-newspaper-variant-multiple">
 
         <template v-slot:activator="{props}">
-          <v-list-item v-bind="props" title="Newsletter" value="newsletter"></v-list-item>
+          <v-list-item v-bind="props" title="Newsletter"></v-list-item>
         </template>
 
-        <v-list-item to="/subscribers" title="Subscribers" value="subscribers"></v-list-item>
+        <v-list-item to="/subscribers" title="Subscribers"></v-list-item>
 
-        <v-list-item to="/news" title="News" value="news"></v-list-item>
+        <v-list-item to="/news" title="News"></v-list-item>
 
       </v-list-group>
 
 
-      <v-list-item to="/support" prepend-icon="mdi-face-agent" title="Support" value="support"></v-list-item>
+      <v-list-item to="/support" prepend-icon="mdi-face-agent" title="Support"></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
